1bd247289134d8d8a15ab537f24386607007ab54
ainul <ainul4147@gmail.com>
2019-12-26 15:33:38 +0600

    Added a new methood called display to ainul.gava

--- ainul.java
+++ ainul.java
@@ -1,6 +1,10 @@
 public class Hollo {
     public static void main (string[]args){
         system.out.printIn("Vs code is Awesome.........")
+        greeting("M Ainul")
     }
-    
+
+    public static void greeting(sring name){
+        system.out.printIn("Welcome" + name);
+        }
     }
